3dc8709f859148eff57e22713a921ec3095cd631
Joaquim <joaquim.simionatop@gmail.com>
2026-09-09 21:19:11 -0300

    Login e Mensagens concluidos

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+import pandas as pd 
+import os 
+st.set_page_config( 
+    page_title=("Tela de Cadastro"), 
+    page_icon="👨‍🔧", 
+    layout="centered" 
+)
+
+st.title("👨‍🔧Cadastro de Alunos") 
+
+nome = st.text_input("Digite o nome do aluno") 
+endereco = st.text_input("Digite o endereço do aluno") 
+dt_nasc = st.date_input("Selecione a data de nascimento", format="DD/MM/YYYY") 
+tipo_aluno = st.selectbox("Selecione o aluno", ["Parcial","Integral" ]) 
+cadastrar = st.button("Cadastrar Aluno") 
+
+
+
+if cadastrar: 
+    if cadastrar and nome=="" or endereco=="" or dt_nasc=="" or tipo_aluno=="":
+     st.error('Usuário inválido!')
+    else:
+     with open("cad_alunos.csv", "a", encoding="utf8") as arquivo: 
+        arquivo.write(f"{nome},{endereco},{dt_nasc},{tipo_aluno}\n") 
+        st.success("Aluno cadastrado com sucesso!") 
+elif nome=="" or endereco=="" or dt_nasc=="" or tipo_aluno=="":
+        st.warning("Preencha todos os campos!")
